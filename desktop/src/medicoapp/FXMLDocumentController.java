18000d9357290e7fa2700e82eb5acadc877716cf
José Daniel Díaz <josed1305@gmail.com>
2016-06-03 14:15:43 -0400

    Integrated Rusben & JD Desktop files

--- desktop/src/medicoapp/FXMLDocumentController.java
+++ desktop/src/medicoapp/FXMLDocumentController.java
@@ -5,6 +5,7 @@
  */
 package medicoapp;
 
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
@@ -17,11 +18,15 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 /**
  *
@@ -47,15 +52,26 @@ public class FXMLDocumentController implements Initializable {
     TableColumn<Appointment, String> motivoColumn;
     @FXML
     TableColumn<Appointment, String> apellidoColumn;
-    @FXML
-    private DatePicker Calendario;
+    // @FXML
+    DatePicker date;
     @FXML
     private VBox Vmenu;
-    
+    @FXML
+    private AnchorPane pacientes_pane; 
+    @FXML
+    private Label nombre_p_label;
+    @FXML
+    private Label apellido_p_label;
+    @FXML
+    private Label cedula_p_label;
+    @FXML
+    private Label email_p_label;
+    @FXML
+    private Label telefono_p_label;
     
     @FXML
     private void crearEvento(ActionEvent event) {
-        System.out.println("Se agrega un elemento a la lista");
+        System.out.println("Se abre la ventana para agregar un elemento a la lista");
         crearCita cr = new crearCita();
         cr.display(tableCitas);
     }
@@ -63,10 +79,11 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void eliminarEvento(ActionEvent event) throws IOException{
+        System.out.println("Se abre la ventana para eliminar un elemento de la lista");
             ObservableList<Appointment> productosSelect, allProductos;
             allProductos = tableCitas.getItems();
             productosSelect = tableCitas.getSelectionModel().getSelectedItems();
-           // productosSelect.forEach(allProductos::remove);
+                // POST: eleminar una cita del dia
                 Appointment np = new Appointment();
                 np.setStart(productosSelect.get(0).getStart());
                 np.setSlot(productosSelect.get(0).getSlot());
@@ -74,16 +91,24 @@ public class FXMLDocumentController implements Initializable {
                 np.setDescription("");
                 allProductos.set(tableCitas.getSelectionModel().getSelectedIndex(), np);
 }
-   
-    @FXML
-    private void verificarSolicitudHTTP(ActionEvent event) {
-        //HTTPRequest.updateDay("5751b61187f7efac09c03f8d");
-    }
     
+    @FXML
+    private void agregarPaciente(ActionEvent event) {
+        System.out.println("Se abre la ventana para agregar un paciente a la BD");
+        Custom_control_agregarPacienteController ap = new Custom_control_agregarPacienteController();
+        ap.display();
+    }
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Inicia el calendario con la fecha actual
-        Calendario.setValue(LocalDate.now());
+        // Se inicializa el calendario y el popup
+        DatePicker dp = new DatePicker(LocalDate.now());
+        DatePickerSkin datePickerSkin = new DatePickerSkin(dp);
+        date = dp;
+        Node popupContent = datePickerSkin.getPopupContent();
+        Vmenu.getChildren().addAll(popupContent);
+        //System.out.println("fecha :  "+dp.getValue());
+
         // Inicialización de las filas por bloque de horas en la tabla
         tableCitas.setEditable(true);
         Date date = new Date(2016,06,02,00,00,00);
@@ -104,9 +129,36 @@ public class FXMLDocumentController implements Initializable {
         nombreColumn.setCellValueFactory(new PropertyValueFactory<>("patientName"));
         horaColumn.setCellValueFactory(new PropertyValueFactory<>("slot"));
         motivoColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
-        // Manejador de los eventos de edicion de las tablas
+        // Manejador de los eventos de el calendario
+        
+        //GET: obtener todos los dias y ponerlos en un arreglo de appointment
         
         
-    }  
+        dp.setOnAction(e -> {
+        
+               
+        
+        });
+        // Manejador del evento cuando se oprime una fila de la tableCitas
+       tableCitas.setRowFactory( tv -> {
+            TableRow<Appointment> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 1 && (! row.isEmpty()) ) {
+                    Appointment rowData = row.getItem();
+                    
+                    /*
+                    nombre_p_label.setText();
+                    apellido_p_label.setText();
+                    cedula_p_label.setText();
+                    email_p_label.setText();
+                    telefono_p_label.setText();
+                    */
+                    System.out.println(rowData + "\n" + rowData.getPatientName() + "\n" + rowData.getDescription());
+                }
+            });
+            return row ;
+        });
+
+    }
     
 }
