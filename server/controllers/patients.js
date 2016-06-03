d5d4bb0a82bab005b03fe4182fec04f18d8d6780
José Daniel Díaz <josed1305@gmail.com>
2016-06-02 21:44:19 -0400

    Patients model

---
+++ server/controllers/patients.js
@@ -0,0 +1,6 @@
+/*
+* Archivo: routes/patients.js
+* Provee los manejadores de solicitudes HTTP para los pacientes.
+*/
+
+var patients = require('../models/patients.js');
---
+++ server/models/patients.js
@@ -0,0 +1,44 @@
+/*
+* Archivo: models/patients.js
+* Provee el modelo de la estructura de los pacientes en la BD noSQL, con un objeto de Mongoose y lo exporta.
+*/
+
+var mongoose = require('mongoose');
+
+// Contiene el feedback de un tratamiento para un diagnóstico. Subdocumento de diagnóstico.
+var treatmentResultSchema = new mongoose.Schema({
+    Rating: { type: Number }
+});
+
+// Contiene el tratamiento de un diagnóstico. Subdocumento de diagnóstico.
+var treatmentSchema = new mongoose.Schema({
+    medication: { type: String, trim: true },
+    quantity: { type: String, trim: true },
+    duration: { type: String, trim: true },
+    frequency: { type: String, trim: true }
+});
+
+// Contiene un diagnóstico de un paciente. Subdocumento de paciente.
+var diagnosticSchema = new mongoose.Schema({
+    date: { type: Date },
+    diagnostic: { type: String, trim: true },
+    treatment: [treatmentSchema],
+    treatmentResult: treatmentResultSchema
+});
+
+// Contiene la información de los usuarios pacientes, incluyendo su historial médico y feedback de sus tratamientos.
+var patientSchema = new mongoose.Schema({
+    patientID: { type: String, trim: true },
+    name: { type: String, trim: true },
+    birthdate: { type: Date, trim: true },
+    email: { type: String, trim: true },
+    phoneNumber: { type: String, trim: true },
+    sex: { type: String, trim: true },
+    weight: { type: Number },
+    height: { type: Number },
+    medicalBackground: [{ type: String, trim: true }],
+    allergies: [{ type: String, trim: true }],
+    diagnostics: [diagnosticSchema]
+});
+
+exports = module.exports = mongoose.model('Patient', patientSchema);
